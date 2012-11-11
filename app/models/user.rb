2577b8c6ec0dea2d6b547a934dd75bd5186d2cc7
Jasmina Atanassova <jasminaata@gmail.com>
2012-11-10 16:06:04 -0800

    Adding unique non-case sensitive validation for user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
   attr_accessible :name, :password, :password_confirmation
 
-  validates :name, uniqueness: true, presence: true
-  validates :password, presence: true, confirmation: true
-  validates :password_confirmation, presence: true
+  validates :name, uniqueness: { case_sensitive: false}, presence: true
+  validates :password, presence: true, confirmation: { :unless => Proc.new { |a| a.password.blank? } }
 
   has_many :posts
   has_many :comments
